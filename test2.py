4da2a44f967a57942a28542611a2b222618f4737
Derek Coffman <derek.coffman@noaa.gov>
2018-10-22 12:25:37 -0700

    Initial commit

---
+++ test2.py
@@ -0,0 +1,6 @@
+# @Author: derek
+# @Date:   2018-10-19T13:02:55-07:00
+# @Last modified by:   derek
+# @Last modified time: 2018-10-19T13:03:10-07:00
+
+print('hello again')
---
+++ test3.py
@@ -0,0 +1,9 @@
+# @Author: derek
+# @Date:   2018-10-19T13:04:09-07:00
+# @Last modified by:   derek
+# @Last modified time: 2018-10-22T12:03:28-07:00
+
+import time
+
+t = time.time()
+print(t)
