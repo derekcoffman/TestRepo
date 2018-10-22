f69d5ae1468e28d05dd6513183c8b78eb913b251
Derek Coffman <derek.coffman@noaa.gov>
2018-10-22 13:51:37 -0700

    Fixed world message
    Co-Authored-By: DerekC <djcoff@gmail.com>

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # @Author: derek
 # @Date:   2018-10-19T12:56:22-07:00
 # @Last modified by:   derek
-# @Last modified time: 2018-10-19T12:57:17-07:00
+# @Last modified time: 2018-10-22T13:51:22-07:00
 
 print('hello world')
+print('the world is not enough')
