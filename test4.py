76196b4ba041b908fe30acb8f1e90039729d90a6
Derek Coffman <derek.coffman@noaa.gov>
2018-10-22 12:33:29 -0700

    Added test4.py

---
+++ test4.py
@@ -0,0 +1,9 @@
+# @Author: derek
+# @Date:   2018-10-22T12:32:33-07:00
+# @Last modified by:   derek
+# @Last modified time: 2018-10-22T12:32:58-07:00
+
+from datetime import datetime
+
+dt = datetime.utcnow()
+print(dt)
