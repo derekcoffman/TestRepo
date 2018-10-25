7f4da542403342b86f07f7bfc2929d72aa7eb5cb
Derek Coffman <derek.coffman@noaa.gov>
2018-10-25 11:00:32 -0700

    first commit with git-cola
    This is only a test....

--- test6.py
+++ test6.py
@@ -1,7 +1,7 @@
 # @Author: derek
 # @Date:   2018-10-22T13:40:24-07:00
 # @Last modified by:   derek
-# @Last modified time: 2018-10-24T14:04:56-07:00
+# @Last modified time: 2018-10-25T10:55:33-07:00
 
 print('test6')
 
